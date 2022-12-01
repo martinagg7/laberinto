f5fde102c9099f07f0b789e02df5894851a9be1b
Martina Garcia <martinagg@myuax.com>
2022-12-01 11:25:33 +0100

    .

--- laberinto.py
+++ laberinto.py
@@ -1,16 +1,16 @@
-
-muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
-laberinto=[]
-for i in range(5):
-    fila=[]
-    for j in range(5):
-        print(i,j)
-        tupla=(i,j)
-        print(tupla[0])
-        if tupla in muro:
-            fila.append("x")
-        else:
-            fila.append(' ')
-    laberinto.append(fila)
-for i in range(5):
-    print(laberinto[i])
+def laberintof():
+    muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
+    laberinto=[]
+    for i in range(5):
+        fila=[]
+        for j in range(5):
+            tupla=(i,j)
+            if tupla in muro:
+                fila.append("x")
+            else:
+                fila.append(' ')
+        laberinto.append(fila)
+    return laberinto
+if __name__=="__main__":
+    for i in range(5):
+        print(laberintof()[i])
